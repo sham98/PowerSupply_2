ba9f1ea6a793f1c0cf0cf1b89446ef1f23f80b70
sham98 <john.souri98@gmail.com>
2022-11-23 10:07:51 +0330

    برگرداندن اولویت

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -664,7 +664,7 @@ static void MX_DMA_Init(void)
 
   /* DMA interrupt init */
   /* DMA1_Channel1_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 1, 0);
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 3, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 
 }
