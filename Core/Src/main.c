1a7577b93975d820931892b1b62e264094ce2b0a
sham98 <john.souri98@gmail.com>
2022-12-14 13:19:01 +0330

    Change PWM resolution

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -648,7 +648,7 @@ static void MX_TIM17_Init(void)
   htim17.Instance = TIM17;
   htim17.Init.Prescaler = 0;
   htim17.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim17.Init.Period = 3200;
+  htim17.Init.Period = 6400;
   htim17.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim17.Init.RepetitionCounter = 0;
   htim17.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
